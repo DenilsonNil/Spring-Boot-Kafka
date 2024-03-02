710f97de17ba04a978d9b8e82aec173bab6d721e
d.alves <d.alves@unlimint.com>
2024-03-01 22:42:52 -0300

    First commit

---
+++ library-event-producer/src/main/java/br/com/kualit/App.java
@@ -0,0 +1,11 @@
+package br.com.kualit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
