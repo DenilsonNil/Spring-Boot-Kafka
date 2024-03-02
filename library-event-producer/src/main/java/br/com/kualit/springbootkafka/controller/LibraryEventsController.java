29b60f51a63b3f2b80ca4be6c9b1d27239be9bf4
d.alves <d.alves@unlimint.com>
2024-03-02 00:17:46 -0300

    Add service to send event

--- library-event-producer/src/main/java/br/com/kualit/springbootkafka/controller/LibraryEventsController.java
+++ library-event-producer/src/main/java/br/com/kualit/springbootkafka/controller/LibraryEventsController.java
@@ -1,7 +1,9 @@
 package br.com.kualit.springbootkafka.controller;
 
 import br.com.kualit.springbootkafka.domain.LibraryEvent;
+import br.com.kualit.springbootkafka.service.LibraryEventsProducer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class LibraryEventsController {
 
+    @Autowired
+    private LibraryEventsProducer producer;
+
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
 
         log.info("libraryEvent : {}", libraryEvent);
+        producer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
